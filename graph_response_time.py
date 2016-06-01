64f5d752d918a4992aa80ef73c3cc32c8f2e3e84
Harris, Michael <micharris@gannett.com>
2016-06-01 14:32:50 -0400

    initial commit

---
+++ graph_response_time.py
@@ -0,0 +1,88 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import csv
+import sys
+
+plt.figure(figsize=(18,8), dpi=100)
+plt.xticks(rotation=70)
+
+path = sys.argv[1]
+
+colors = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
+                  '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
+                  '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
+                  '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+
+with open(path+'/responseTime-aggregated.csv', 'rU') as infile:
+  # read the file as a dictionary for each row ({header : value})
+	reader = csv.DictReader(infile)
+	data = {}
+	for row in reader:
+		for header, value in row.items():
+			try:
+				data[header].append(value)
+			except KeyError:
+				data[header] = [value]
+
+#get list of unique endpoints for looping through
+endpointList = list(set(data['endpoint']))
+xData = {}
+for value in endpointList:
+	xData[value] = [];
+
+#get rid of duplicate dates
+data['Date'] = list(set(data['Date']))
+#print data['Date']
+
+#show from oldest test to newest
+data['Date'] =  sorted(data['Date'], key=lambda d: map(int, d.split('-')))
+
+with open(path+'/responseTime-aggregated.csv', 'rU') as infile:
+  # read the file as a dictionary for each row ({header : value})
+	reader = csv.DictReader(infile)
+	for row in reader:
+		for url in endpointList:
+			if row['endpoint'] == url:
+				xData[url].append(int(row['responseTime']))
+
+#get the number of data points - this will be the number of tickpoints on the chart
+x = 0
+num_of_datapoints = []
+while (x < len(xData[endpointList[0]])):
+	num_of_datapoints.append(x)
+	x += 1
+
+#get the highest value response time
+highest = 0
+for valueList in xData.values():
+	for val in valueList:
+		if(val > highest):
+			highest = val 
+
+j = 0
+decrement = 0
+
+for key, val in xData.items():
+	if len(val) > 1:
+		x = 0
+		num_of_datapoints = []
+		for items in val:
+			num_of_datapoints.append(x)
+			x += 1 
+		val.reverse()
+		plt.plot(num_of_datapoints, val, color = colors[j])
+		plt.text((len(num_of_datapoints)-1)*1.02, highest - decrement, key, fontsize=14, color=colors[j])
+
+		if j == 19:
+			j = 0
+		else: 
+			j += 1
+		decrement += highest*.05
+
+plt.xticks(num_of_datapoints, data['Date'])
+plt.title('Response Time of Previous builds')
+plt.ylabel('Median Response Time (milliseconds)', labelpad=20)
+plt.xlabel('Date: Year-Month-Day-Hour-Minute', labelpad=20)
+
+plt.savefig(path+'/load-test-comparison.png', bbox_inches='tight')
--- locust-to-csv.py
+++ locust-to-csv.py
@@ -21,23 +21,53 @@ def convert_to_CSV(path):
 	responseFlag = 0
 	totalErrors = 0
 	totalRequests = 0
+	startFlag = 0
 
 	with open(path+"/locust.out") as f:
 		for line in f:
-			if(re.search('Percentage of the requests.',line)):
-				responseFlag = 1
-			 
+			#get median response times for each endpoint
+			if(responseFlag == 0 and re.search('Median',line)):
+				responseFlag = 2
+
+			if(responseFlag == 3):
+				if(re.search('-------------------.',line)):
+					responseFlag = 0
+				else:	
+					responseTimes = line.split()
+					try:
+						if(responseTimes[8].isdigit()):
+							data[responseTimes[1]] = responseTimes[8]
+
+					except IndexError:
+						print ''
+
 			if(responseFlag == 2 and re.search('-------------------.',line)):
 				responseFlag = 3
 
-			if(responseFlag == 1 and re.search('-------------------.',line)):
-				responseFlag = 2
+			#get error percentages for each endpoint
+			if(re.search('Percentage of the requests.',line)):
+				startFlag = 1
 
-			if(responseFlag == 2):
-				responseTimes = line.split()
+			if(startFlag == 1 and re.search('Name',line)):
+				errorFlag = 1
+
+			if(errorFlag == 2 and re.search('-------------------.',line)):
+				errorFlag = 3
+
+			if(errorFlag == 1 and re.search('-------------------.',line)):
+				errorFlag = 2
+
+			if(re.search('Total',line)):
+				errorFlag = 0
+
+			if(errorFlag == 2):
+				errorData = line.split()
 				try:
-					if(responseTimes[7].isdigit()):
-						data[responseTimes[1]] = responseTimes[7]
+					if(errorData[2].isdigit()):
+						err = errorData[3]
+						err = err.split('(')
+						err = err[1].split('%')
+						errors[errorData[1]] = err[0]
 				except IndexError:
 					print ''
 
@@ -50,6 +80,16 @@ def write_CSV_File(path):
 	    for key, value in data.iteritems():
 	    	writer.writerow([now.strftime("%Y-%m-%d-%H-%M"), key, value])
 
+def write_error_file(path):
+	with open(path+'/errorRate.csv', 'w') as csvfile:
+	    fieldnames = ['date', 'endpoint', 'errorRate']
+	    writer = csv.writer(csvfile)
+
+	    writer.writerow(fieldnames)
+	    for key, value in errors.iteritems():
+	    	writer.writerow([now.strftime("%Y-%m-%d-%H-%M"), key, value])
+
 path = get_file_path()
 convert_to_CSV(path)
 write_CSV_File(path)
+write_error_file(path)
